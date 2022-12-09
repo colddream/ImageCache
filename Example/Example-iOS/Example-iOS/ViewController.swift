9b4ed274acb255a944eb4cb97eb12213f05bd17a
Thang Do <dothang@Mac-mini.routers.saritasa.io>
2022-12-09 10:11:38 +0700

    - Create example

---
+++ Example/Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Do Thang on 09/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
