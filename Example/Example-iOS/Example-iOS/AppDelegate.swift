57acf6fa9934867609c9dd646ce6406a3d7b5c3c
Thang Do <dothang@Mac-mini.routers.saritasa.io>
2022-12-09 10:26:02 +0700

    - Update example

--- Example/Example-iOS/Example-iOS/AppDelegate.swift
+++ Example/Example-iOS/Example-iOS/AppDelegate.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import ImageCache
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Setup ImageCache
+        setupImageCache()
+        
         return true
     }
+    
+    private func setupImageCache() {
+        let taskQueue = OperationQueue()
+        taskQueue.maxConcurrentOperationCount = 1
+        let config = ImageCache.Config(countLimit: 100, memoryLimit: 100 * 1024 * 1024)
+        
+        ImageLoader.shared.config(cache: ImageCache(config: config),
+                                  executeQueue: taskQueue,
+                                  receiveQueue: .main)
+    }
 }
-
--- Example/Example-iOS/Example-iOS/ViewController.swift
+++ Example/Example-iOS/Example-iOS/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import ImageCache
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupImageCache()
+        testing()
     }
 
-
+    private func setupImageCache() {
+        let taskQueue = OperationQueue()
+        taskQueue.maxConcurrentOperationCount = 1
+        let config = ImageCache.Config(countLimit: 100, memoryLimit: 100 * 1024 * 1024)
+        
+        ImageLoader.shared.config(cache: ImageCache(config: config),
+                                  executeQueue: taskQueue,
+                                  receiveQueue: .main)
+    }
+    
+    private func testing() {
+        let imageUrls = [
+//            "https://api.github.com/users/hadley/repos",
+//            "http://ip-api.com/json",
+            "https://api.github.com/repositories/19438/commits",
+            "https://res.cloudinary.com/demo/basketball_shot.jpg",
+            "https://live.staticflickr.com/2912/13981352255_fc59cfdba2_b.jpg",
+            "https://res.cloudinary.com/demo/image/upload/if_ar_gt_3:4_and_w_gt_300_and_h_gt_200,c_crop,w_300,h_200/sample.jpg"
+        ]
+        
+        var finishedCount = 0
+        for urlString in imageUrls {
+            ImageLoader.shared.loadImage(from: URL(string: urlString)!) { result in
+                print("Finished Load for: \(urlString)")
+                switch result {
+                case .success(let image):
+                    print("\(image)")
+                case .failure(let error):
+                    print("\(error.localizedDescription)")
+                }
+                
+                finishedCount += 1
+            }
+        }
+    }
 }
 
